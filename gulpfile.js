9ddf1b73cf887849f31ddcfb98d6958b87cad186
Lia5 <Lia_na5@mail.ru>
2017-06-03 19:58:22 +0300

    head nav stats

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  gulp.src("./sass/style.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("sass:watch", function() {
+  gulp.watch("./sass/**/*.scss", ["sass"]);
+
+});
+
+
+//"./sass/**/*.scss   - текущая папака, и все вложнные с расширением sass
